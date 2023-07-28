524cd87b0feb461abdd2ceb8e208dbce263a2db4
aboura <bouchrabichou638@gmail.com>
2023-07-28 17:01:10 +0200

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,69 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+console.log(cats);
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+  return cats;
+}
+console.log(destructivelyAppendCat('Ralph'));
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+  return cats;
+}
+console.log(destructivelyPrependCat('Bob'));
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+  return cats;
+}
+
+// Create a copy of the 'cats' array before modifying it
+
+cats = destructivelyRemoveLastCat();
+console.log(cats);
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+  return cats;
+}
+
+// Create a copy of the 'cats' array before modifying it
+
+cats = destructivelyRemoveFirstCat();
+
+function appendCat(name) {
+  // Create a copy of the 'cats' array and modify the copy
+  let newArray = [...cats, name];
+  return newArray;
+}
+console.log(appendCat("Broom"));
+
+function prependCat(name) {
+  // Create a copy of the 'cats' array and modify the copy
+  let newArray = [name, ...cats];
+  return newArray;
+}
+console.log(prependCat("Arnold"));
+console.log(cats);
+
+function removeLastCat() {
+  // Create a copy of the 'cats' array and modify the copy
+  let newArray = [...cats];
+  newArray.pop();
+  return newArray;
+}
+
+// Create a copy of the 'cats' array before modifying it
+
+
+
+function removeFirstCat() {
+  // Create a copy of the 'cats' array and modify the copy
+  let newArray = [...cats];
+  newArray.shift();
+  return newArray;
+}
+
+
